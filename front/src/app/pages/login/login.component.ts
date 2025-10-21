d333e83f7646fb6a8813a79e5cc69336c29d891c
EdouardPlantevin <plantevin.contact@gmail.com>
2025-10-21 11:34:47 +0200

    angular form

--- front/src/app/pages/login/login.component.ts
+++ front/src/app/pages/login/login.component.ts
@@ -1,22 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {HeaderComponent} from "../../components/header/header.component";
 import {MatButton} from "@angular/material/button";
 import {RouterLink} from "@angular/router";
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   imports: [
     HeaderComponent,
     MatButton,
-    RouterLink
+    ReactiveFormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  getErrorMessage(fieldName: string): string {
+    const field = this.loginForm.get(fieldName);
+    if (field?.hasError('required')) {
+      return `${this.getFieldLabel(fieldName)} est requis`;
+    }
+    if (field?.hasError('email')) {
+      return 'Format d\'email invalide';
+    }
+    return '';
+  }
+
+  getFieldLabel(fieldName: string): string {
+    const labels: { [key: string]: string } = {
+      username: 'E-mail ou nom d\'utilisateur',
+      password: 'Mot de passe'
+    };
+    return labels[fieldName] || fieldName;
+  }
+
+  isFieldInvalid(fieldName: string): boolean {
+    const field = this.loginForm.get(fieldName);
+    return !!(field && field.invalid && field.touched);
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Formulaire valide:', this.loginForm.value);
+    } else {
+      Object.keys(this.loginForm.controls).forEach(key => {
+        this.loginForm.get(key)?.markAsTouched();
+      });
+      console.log('Formulaire invalide');
+    }
+  }
 
   back() {
     window.history.back();
   }
-
 }
--- front/src/app/pages/register/register.component.ts
+++ front/src/app/pages/register/register.component.ts
@@ -1,19 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {HeaderComponent} from "../../components/header/header.component";
 import {MatButton} from "@angular/material/button";
-import {RouterLink} from "@angular/router";
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
     imports: [
         HeaderComponent,
         MatButton,
-        RouterLink
+        ReactiveFormsModule
     ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.registerForm = this.fb.group({
+      username: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  getErrorMessage(fieldName: string): string {
+    const field = this.registerForm.get(fieldName);
+    if (field?.hasError('required')) {
+      return `${this.getFieldLabel(fieldName)} est requis`;
+    }
+    if (field?.hasError('email')) {
+      return 'Format d\'email invalide';
+    }
+    return '';
+  }
+
+  getFieldLabel(fieldName: string): string {
+    const labels: { [key: string]: string } = {
+      username: 'Nom d\'utilisateur',
+      email: 'Adresse e-mail',
+      password: 'Mot de passe'
+    };
+    return labels[fieldName] || fieldName;
+  }
+
+  isFieldInvalid(fieldName: string): boolean {
+    const field = this.registerForm.get(fieldName);
+    return !!(field && field.invalid && field.touched);
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Formulaire valide:', this.registerForm.value);
+    } else {
+      Object.keys(this.registerForm.controls).forEach(key => {
+        this.registerForm.get(key)?.markAsTouched();
+      });
+      console.log('Formulaire invalide');
+    }
+  }
 
   back() {
     window.history.back();
